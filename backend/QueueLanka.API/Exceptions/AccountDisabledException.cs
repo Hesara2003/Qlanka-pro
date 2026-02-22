4d1ea558215b36fb3dc6b5e4d961d80bf9acb0ca
Hesara2003 <it23596702@my.sliit.lk>
2026-02-22 22:39:07 +0530

    SCRUM-13: Add domain exceptions (AppException, DuplicateUsername, DuplicateEmail, InvalidCredentials, AccountDisabled)

---
+++ backend/QueueLanka.API/Exceptions/AccountDisabledException.cs
@@ -0,0 +1,7 @@
+namespace QueueLanka.API.Exceptions;
+
+public class AccountDisabledException : AppException
+{
+    public AccountDisabledException()
+        : base(403, "ACCOUNT_DISABLED", "This account has been deactivated.") { }
+}
