bcc1772d8828e10f427cc86225abf6c11f5a1d1a
Hesara2003 <it23596702@my.sliit.lk>
2026-02-27 22:23:07 +0530

    feat: Remove unused translation import from BookingPage component

--- frontend/src/pages/BookingPage.tsx
+++ frontend/src/pages/BookingPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 import { useAuth } from "../context/AuthContext";
 import { getServiceCenterById } from "../api/serviceCenterApi";
 import { bookToken } from "../api/appointmentApi";
