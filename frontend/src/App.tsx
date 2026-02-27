b456d9468846e40bab3e107f73b4f38267a8bc0c
Hesara2003 <it23596702@my.sliit.lk>
2026-02-27 09:29:49 +0530

    feat: add route for ServiceCentersPage in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
+import ServiceCentersPage from "./pages/ServiceCentersPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/service-centers" element={<ServiceCentersPage />} />
     </Routes>
   );
 }
