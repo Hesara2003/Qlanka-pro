c12995d6ebe676870ba4c41d2f31bc99b7fe7e03
Hesara2003 <it23596702@my.sliit.lk>
2026-02-27 09:58:38 +0530

    feat: import i18n configuration for localization support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App.tsx";
 import "./index.css";
+import "./i18n/config";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
