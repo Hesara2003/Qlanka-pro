ff4aa9304ea83348ea09d4fef7e7b29cfed656e0
Hesara2003 <it23596702@my.sliit.lk>
2026-03-04 17:04:39 +0530

    feat: Implement new notification service with event bus integration and a Dockerfile for containerization.

--- backend/QueueLanka.Notification/Program.cs
+++ backend/QueueLanka.Notification/Program.cs
@@ -1,7 +1,27 @@
-using QueueLanka.Notification;
+using QueueLanka.Shared.Events;
+using QueueLanka.Notification.Services;
+using QueueLanka.Notification.Handlers;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+// Register services
+builder.Services.AddSingleton<IEmailService, SmtpEmailService>();
+builder.Services.AddSingleton<INotificationService, NotificationService>();
+
+// Register Event Bus (using the simple InMemoryEventBus for MVP)
+builder.Services.AddSingleton<IEventBus, InMemoryEventBus>();
+
+// Register Event Handlers
+builder.Services.AddTransient<UserRegisteredEventHandler>();
+builder.Services.AddTransient<BookingConfirmedEventHandler>();
+builder.Services.AddTransient<TokenCancelledEventHandler>();
 
 var host = builder.Build();
-host.Run();
+
+// Subscribe to events
+var eventBus = host.Services.GetRequiredService<IEventBus>();
+eventBus.Subscribe<UserRegisteredEvent, UserRegisteredEventHandler>();
+eventBus.Subscribe<BookingConfirmedEvent, BookingConfirmedEventHandler>();
+eventBus.Subscribe<TokenCancelledEvent, TokenCancelledEventHandler>();
+
+await host.RunAsync();
